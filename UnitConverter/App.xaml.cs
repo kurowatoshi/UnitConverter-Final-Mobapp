18df872915669922fa1082bebe3c3b15114089bd
ACT-STUDENT <ACT-STUDENT@LAB6-PC2>
2024-11-27 16:17:12 +0800

    Add project files.

---
+++ UnitConverter/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using UnitConverter.MVVM.Views;
+
+namespace UnitConverter;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new NavigationPage(new MenuView());
+	}
+}
